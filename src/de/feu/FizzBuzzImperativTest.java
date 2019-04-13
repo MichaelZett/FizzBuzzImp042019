87779672f5c29a1c1affeae5f4edd0f1a42f88ac
MichaelZett <michael2.zoeller@gmail.com>
2019-04-13 12:22:03 +0200

    Initial commit

---
+++ src/de/feu/FizzBuzzImperativTest.java
@@ -0,0 +1,22 @@
+package de.feu;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class FizzBuzzImperativTest {
+	private FizzBuzzImperativ testee;
+
+	@Before
+	public void setUp() throws Exception {
+		testee = new FizzBuzzImperativ();
+	}
+
+	@Test
+	public void shouldSayFizz() {
+		assertThat(testee.determineWord(3), is("Fizz"));
+	}
+
+}
